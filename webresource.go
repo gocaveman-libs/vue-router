9d005c31d66d0e6bb40e998c9d8ee99b27161eed
Brad Peabody <brad@peabody.io>
2018-06-07 17:20:32 -0700

    import version 3.0.0

--- webresource.go
+++ webresource.go
@@ -1,4 +1,4 @@
 package vuerouter
 
-//go:generate mkwebresource -R -p "github.com/gocaveman-libs/vue-router" .
+//go:generate mkwebresource -R -p "github.com/gocaveman-libs/vue-router" -r "github.com/gocaveman-libs/vue" .
 
